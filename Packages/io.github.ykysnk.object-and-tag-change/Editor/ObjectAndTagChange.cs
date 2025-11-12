27e3a25d6f18157abe434ff47700e5e9a2e3bbb5
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-11-12 10:22:28 +0800

    refactor: Use file-scoped namespace and adjust formatting in ObjectAndTagChange.cs

--- Packages/io.github.ykysnk.object-and-tag-change/Editor/ObjectAndTagChange.cs
+++ Packages/io.github.ykysnk.object-and-tag-change/Editor/ObjectAndTagChange.cs
@@ -3,70 +3,69 @@ using UnityEditor;
 using UnityEngine;
 
 // Refs: https://booth.pm/ja/items/6475280
-namespace io.github.ykysnk.ObjectAndTagChange.Editor
+namespace io.github.ykysnk.ObjectAndTagChange.Editor;
+
+public class SetObjectAndTag : EditorWindow
 {
-    public class SetObjectAndTag : EditorWindow
+    private const string EditorOnlyTag = "EditorOnly";
+    private const string UntaggedTag = "Untagged";
+    private const string MenuPath = "Tools/Set Object and Tag #e";
+    private const string MenuPath2 = "GameObject/yky/Set Object and Tag";
+    private static readonly Dictionary<int, string> OriginalTags = new();
+    private static readonly Dictionary<int, bool> WasActives = new();
+
+    [MenuItem(MenuPath)]
+    [MenuItem(MenuPath2)]
+    private static void ToggleInactiveAndTag(MenuCommand menuCommand)
     {
-        private static readonly Dictionary<int, string> OriginalTags = new();
-        private static readonly Dictionary<int, bool> WasActives = new();
-        private const string EditorOnlyTag = "EditorOnly";
-        private const string UntaggedTag = "Untagged";
-        private const string MenuPath = "Tools/Set Object and Tag #e";
-        private const string MenuPath2 = "GameObject/yky/Set Object and Tag";
+        if (!ShouldExecute(menuCommand)) return;
 
-        [MenuItem(MenuPath)]
-        [MenuItem(MenuPath2)]
-        private static void ToggleInactiveAndTag(MenuCommand menuCommand)
+        var selectedObjects = Selection.gameObjects;
+
+        if (selectedObjects.Length < 1)
         {
-            if (!ShouldExecute(menuCommand)) return;
-
-            var selectedObjects = Selection.gameObjects;
-
-            if (selectedObjects.Length < 1)
-            {
-                Debug.LogWarning("オブジェクトが選択されていません。");
-                return;
-            }
-
-            foreach (var obj in selectedObjects)
-            {
-                var id = obj.GetInstanceID();
-
-                if (obj.activeSelf)
-                {
-                    if (!OriginalTags.ContainsKey(id))
-                    {
-                        OriginalTags.TryAdd(id, obj.tag);
-                        WasActives.TryAdd(id, obj.activeSelf);
-                    }
-
-                    Undo.RecordObject(obj, "Change EditorOnly");
-                    obj.SetActive(false);
-                    obj.tag = EditorOnlyTag;
-                }
-                else
-                {
-                    var hasOriginalTag = OriginalTags.TryGetValue(id, out var originalTag);
-                    var hasActive = WasActives.TryGetValue(id, out var wasActive);
-
-                    Undo.RecordObject(obj, "Undo Tag");
-                    obj.SetActive(!hasActive || wasActive);
-                    if (obj.CompareTag(EditorOnlyTag))
-                        obj.tag = hasOriginalTag && originalTag != EditorOnlyTag ? originalTag : UntaggedTag;
-
-                    OriginalTags.Remove(id);
-                    WasActives.Remove(id);
-                }
-
-                EditorUtility.SetDirty(obj);
-            }
+            Debug.LogWarning("オブジェクトが選択されていません。");
+            return;
         }
 
-        // Refs: https://discussions.unity.com/t/how-to-execute-menuitem-for-multiple-objects-once/91492/5
-        private static bool ShouldExecute(MenuCommand menuCommand)
+        foreach (var obj in selectedObjects)
         {
-            if (menuCommand.context == null) return true;
-            return menuCommand.context == Selection.activeObject;
+            var id = obj.GetInstanceID();
+
+            if (obj.activeSelf)
+            {
+                if (!OriginalTags.ContainsKey(id))
+                {
+                    OriginalTags.TryAdd(id, obj.tag);
+                    WasActives.TryAdd(id, obj.activeSelf);
+                }
+
+                Undo.RecordObject(obj, "Change EditorOnly");
+                obj.SetActive(false);
+                obj.tag = EditorOnlyTag;
+            }
+            else
+            {
+                var hasOriginalTag = OriginalTags.TryGetValue(id, out var originalTag);
+                var hasActive = WasActives.TryGetValue(id, out var wasActive);
+
+                Undo.RecordObject(obj, "Undo Tag");
+                obj.SetActive(!hasActive || wasActive);
+                if (obj.CompareTag(EditorOnlyTag))
+                    obj.tag = hasOriginalTag && originalTag != EditorOnlyTag ? originalTag ?? UntaggedTag : UntaggedTag;
+
+                OriginalTags.Remove(id);
+                WasActives.Remove(id);
+            }
+
+            EditorUtility.SetDirty(obj);
         }
+    }
+
+    // Refs: https://discussions.unity.com/t/how-to-execute-menuitem-for-multiple-objects-once/91492/5
+    private static bool ShouldExecute(MenuCommand menuCommand)
+    {
+        if (menuCommand.context == null) return true;
+        return menuCommand.context == Selection.activeObject;
     }
 }
